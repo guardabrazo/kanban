7d74d346b6ef0171dfadad7b2378262861b7cab6
Guardabrazo <guardabrazo@Guardabrazos-MacBook-Pro.local>
2025-12-07 16:04:55 +0100

    Header fix mobile

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/kanban/',
+  resolve: {
+    alias: {
+      'grdbrz-ui': path.resolve(__dirname, 'lib/grdbrz-ui/src/lib/index.ts')
+    }
+  }
 })
